49822649c7f7087da16d13d5990be79ee16f5ceb
Cristian Hoffmann <cristianhof@gmail.com>
2025-04-19 09:10:42 +0200

    refactor

--- backend/travelAgent/src/main/java/com/crishof/travelagent/TravelAgentApplication.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/TravelAgentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelAgentApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TravelAgentApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgentApplication.class, args);
+	}
 
 }
