ef8810b0fb6edffe906e365b818eab045e77800e
crishof <cristianhof@gmail.com>
2025-04-19 11:11:50 +0200

    add components

--- frontend/travel-agent-ui/src/app/app.component.ts
+++ frontend/travel-agent-ui/src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./layout/navbar/navbar.component";
 import { FooterComponent } from "./layout/footer/footer.component";
+import { AgentComponent } from "./features/agent/agent.component";
+import { CustomerComponent } from "./features/customer/customer.component";
+import { SaleComponent } from "./features/sale/sale.component";
+import { SupplierComponent } from "./features/supplier/supplier.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AgentComponent, CustomerComponent, SaleComponent, SupplierComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
