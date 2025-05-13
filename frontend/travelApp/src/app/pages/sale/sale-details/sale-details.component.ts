9a78e500f171e82244bda8aa78d1b289e190274c
Cristian Hoffmann <cristianhof@gmail.com>
2025-05-13 11:15:49 +0200

    show payments in sale details

--- frontend/travelApp/src/app/pages/sale/sale-details/sale-details.component.ts
+++ frontend/travelApp/src/app/pages/sale/sale-details/sale-details.component.ts
@@ -5,6 +5,8 @@ import { CommonModule, Location } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PaymentService } from '../../../services/payment.service';
+import { CustomerPaymentService } from '../../../services/customer-payment.service';
+import { ICustomerPayment } from '../../../model/customerPayment.model';
 
 @Component({
   selector: 'app-sale-details',
@@ -19,11 +21,13 @@ export class SaleDetailsComponent implements OnInit {
   saleId: string | null = null;
 
   sale: any;
-  payments: any[] = [];
+  payments: ICustomerPayment[] = [];
   pendingBalance: number = 0;
   readonly _saleService = inject(SaleService);
   readonly _paymentService = inject(PaymentService);
   readonly _location = inject(Location);
+  readonly _customerPaymentService = inject(CustomerPaymentService);
+  isLoading: boolean = true;
 
   ngOnInit() {
     const saleId = this.route.snapshot.paramMap.get('id');
@@ -33,25 +37,36 @@ export class SaleDetailsComponent implements OnInit {
     }
   }
 
+  loadPayments(saleId: number, customerId: number): void {
+    this._customerPaymentService
+      .getPaymentsByCustomerAndSaleId(customerId, saleId)
+      .subscribe({
+        next: (data: ICustomerPayment[]) => {
+          this.payments = data; // Asigna la lista de pagos a la propiedad `payments`
+          this.calculatePendingBalance(); // Calcula el saldo pendiente después de cargar los pagos
+        },
+        error: (error) => {
+          console.error('Error loading payments:', error);
+        },
+      });
+  }
+
   loadSaleDetails(saleId: number): void {
+    console.log('Loading sale details for ID:', saleId);
     this._saleService.getSaleById(saleId).subscribe({
       next: (data) => {
         this.sale = data;
 
+        if (this.sale.customerResponse) {
+          // Una vez que se cargan los detalles de la venta, carga los pagos
+          this.loadPayments(saleId, this.sale.customerResponse.id);
+        }
+
         this.calculatePendingBalance();
+        this.isLoading = false;
       },
       error: (error) => {
         console.error('Error loading sale details:', error);
-      },
-    });
-
-    this._paymentService.getPaymentsBySaleId(saleId).subscribe({
-      next: (data) => {
-        this.payments = data;
-        this.calculatePendingBalance();
-      },
-      error: (error) => {
-        console.error('Error loading payments:', error);
       },
     });
   }
---
+++ frontend/travelApp/src/app/services/customer-payment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ICustomerPayment } from '../model/customerPayment.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomerPaymentService {
+  constructor() {}
+
+  readonly _http = inject(HttpClient);
+  readonly _urlBase = `http://localhost:9001/customerPayment`;
+
+  getPaymentsByCustomerAndSaleId(
+    customerId: number,
+    saleId: number
+  ): Observable<ICustomerPayment[]> {
+    return this._http.get<ICustomerPayment[]>(
+      `${this._urlBase}/customers/${customerId}/travel/${saleId}/payments`
+    );
+  }
+}
