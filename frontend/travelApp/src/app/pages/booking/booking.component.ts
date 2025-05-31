dba46b306e4302c43622de678b0a26d4cdb27535
Cristian Hoffmann <cristianhof@gmail.com>
2025-05-31 10:59:55 +0200

    payment modal in booking component

--- frontend/travelApp/src/app/pages/booking/booking.component.ts
+++ frontend/travelApp/src/app/pages/booking/booking.component.ts
@@ -8,11 +8,20 @@ import { SupplierService } from '../../services/supplier.service';
 import { ISupplier } from '../../model/supplier.model';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest } from 'rxjs';
+import { PaymentModalComponent } from '../../utils/payment-modal/payment-modal.component';
+import { PaymentService } from '../../services/payment.service';
+import { IPayment } from '../../model/payment.model';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    PaymentModalComponent,
+  ],
 
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css',
@@ -22,6 +31,13 @@ export class BookingComponent implements OnInit {
   readonly _bookingService = inject(BookingService);
   readonly _supplierService = inject(SupplierService);
   readonly _route = inject(ActivatedRoute);
+  readonly _paymentService = inject(PaymentService);
+  hoveredBookingId: number | null = null;
+  showPaymentModal: boolean = false;
+  selectedBookingId?: number;
+  selectedSupplierName?: string;
+  selectedSupplierCurrency?: string;
+  selectedBookingNumber?: string;
 
   ngOnInit(): void {
     combineLatest([
@@ -50,5 +66,42 @@ export class BookingComponent implements OnInit {
         },
       });
     });
+  }
+
+  openPaymentModal(booking: IBooking) {
+    this.selectedBookingId = booking.id;
+    this.selectedSupplierName = booking.supplierName;
+    this.selectedBookingNumber = booking.bookingNumber;
+    this.selectedSupplierCurrency = booking.currency;
+    this.showPaymentModal = true;
+  }
+  closePaymentModal(): void {
+    this.showPaymentModal = false;
+    this.selectedBookingId = undefined;
+  }
+  onPaymentRegistered(paymentData: any): void {
+    const payment: IPayment = {
+      bookingId: paymentData.bookingId,
+      amount: paymentData.amount,
+      paymentDate: new Date().toISOString(),
+      currency: this.selectedSupplierCurrency ?? '',
+      description: paymentData.description,
+    };
+    this._paymentService.savePayment(payment).subscribe({
+      next: (response) => {
+        console.log('Payment saved successfully:', response);
+        const booking = this.bookingList.find(
+          (b) => b.id === payment.bookingId
+        );
+        if (booking) {
+          booking.paid = true;
+        }
+      },
+      error: (error) => {
+        console.error('Error saving payment:', error);
+      },
+    });
+    console.log('Payment registered:', paymentData);
+    this.closePaymentModal();
   }
 }
