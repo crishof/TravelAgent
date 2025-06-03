0bb1d0aac04c8cc07956a65bff70fec8c58bca4b
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-03 22:40:24 +0200

    fix: spa fallback

--- backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
@@ -3,11 +3,19 @@ package com.crishof.travelagent.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class SpaController {
 
-    @GetMapping(value = "/{path:^(?!api)(?!.*\\.).*$}")
+    /**
+     * Redirige todas las rutas no API y sin extensión a index.html
+     * para que Angular maneje el enrutamiento.
+     * <p>
+     * Evita rutas como:
+     * - /api/...
+     * - /main.js, /styles.css, etc.
+     * - /index.html (para prevenir StackOverflowError)
+     */
+    @GetMapping(value = "/{path:^(?!api)(?!index\\.html)(?!.*\\.).*$}")
     public String redirect() {
         return "forward:/index.html";
     }
