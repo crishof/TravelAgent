47929b489779f11574fa862c0186bc6b32ff866e
crishof <cristianhof@gmail.com>
2025-06-03 22:19:28 +0200

    fix spaController

--- backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = {"/{path:^(?!api)(?!.*\\.).*$}"})
+    @RequestMapping(value = {"/{path:^(?!api$)(?!api\\/).*$}"})
     public String redirect() {
         return "forward:/index.html";
     }
