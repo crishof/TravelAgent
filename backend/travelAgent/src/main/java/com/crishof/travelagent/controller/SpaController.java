f45bdda6fd5afa5919a7f5be037ef02593a59530
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-03 11:36:25 +0200

    fix spa controller

--- backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
@@ -1,15 +1,14 @@
 package com.crishof.travelagent.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = {"/{path:[^\\.]*}"})
-    public String redirect(@PathVariable(required = false) String path) {
+    @RequestMapping(value = {"/{path:^(?!api)(?!.*\\.).*$}"})
+    public String redirect() {
         return "forward:/index.html";
     }
 }
