1f8710ca96b2cbc626bd31246488e3e60f5ba09c
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-03 22:56:07 +0200

    new build

--- backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
@@ -1,21 +1,15 @@
 package com.crishof.travelagent.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
 
-    /**
-     * Redirige todas las rutas no API y sin extensión a index.html
-     * para que Angular maneje el enrutamiento.
-     * <p>
-     * Evita rutas como:
-     * - /api/...
-     * - /main.js, /styles.css, etc.
-     * - /index.html (para prevenir StackOverflowError)
-     */
-    @GetMapping(value = "/{path:^(?!api)(?!index\\.html)(?!.*\\.).*$}")
+    @RequestMapping({
+            "/{path:^(?!api$)(?!api/)(?!index\\.html$)(?!.*\\.).*$}",
+            "/**/{path:^(?!api$)(?!api/)(?!index\\.html$)(?!.*\\.).*$}"
+    })
     public String redirect() {
         return "forward:/index.html";
     }
