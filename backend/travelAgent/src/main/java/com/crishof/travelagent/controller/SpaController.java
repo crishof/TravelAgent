fb4ab9280b3f08301932af8cc1161d20e95a1ae0
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-03 11:26:32 +0200

    add spaController

--- backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
@@ -1,4 +1,15 @@
 package com.crishof.travelagent.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class SpaController {
+
+    @RequestMapping(value = {"/{path:[^\\.]*}"})
+    public String redirect(@PathVariable(required = false) String path) {
+        return "forward:/index.html";
+    }
 }
