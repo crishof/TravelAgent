dfad38ea18c13f2771ab6b9d797103e953343471
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-03 23:07:42 +0200

    test

--- backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/controller/SpaController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SpaController {
 
     @RequestMapping({
-            "/{path:^(?!api$)(?!api/)(?!index\\.html$)(?!.*\\.).*$}",
-            "/**/{path:^(?!api$)(?!api/)(?!index\\.html$)(?!.*\\.).*$}"
+//            "/{path:^(?!api$)(?!api/)(?!index\\.html$)(?!.*\\.).*$}",
+//            "/**/{path:^(?!api$)(?!api/)(?!index\\.html$)(?!.*\\.).*$}"
     })
     public String redirect() {
         return "forward:/index.html";
