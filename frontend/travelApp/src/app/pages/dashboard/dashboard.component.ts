e99e4d12e8d1073c6f3026db30983660c2f3ac20
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-08 15:05:51 +0200

    add dashboard

---
+++ frontend/travelApp/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent implements OnInit {
+  totalSales = 125000;
+  pendingPayments = 18500;
+  totalCustomers = 320;
+
+  recentSales = [
+    { date: new Date(), customer: 'John Doe', amount: 1200, status: 'Paid' },
+    {
+      date: new Date(),
+      customer: 'Jane Smith',
+      amount: 950,
+      status: 'Pending',
+    },
+    { date: new Date(), customer: 'Carlos Ruiz', amount: 2100, status: 'Paid' },
+    { date: new Date(), customer: 'Anna Lee', amount: 750, status: 'Pending' },
+  ];
+
+  topSuppliers = [
+    { name: 'Global Travels', bookings: 42 },
+    { name: 'Sunshine Tours', bookings: 37 },
+    { name: 'Adventure Co.', bookings: 29 },
+  ];
+
+  topAgents = [
+    { name: 'Alice Brown', sales: 34, total: 34000 },
+    { name: 'Test Brown', sales: 34, total: 34000 },
+    { name: 'Bob White', sales: 28, total: 28000 },
+    { name: 'Maria Green', sales: 22, total: 22000 },
+  ];
+
+  ngOnInit(): void {
+    // TODO Inicializar datos
+  }
+}
