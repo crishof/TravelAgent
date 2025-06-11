7c288d7b9b1f4b9019f96e12d4445374ab88a249
Cristian Hoffmann <cristianhof@gmail.com>
2025-06-11 22:48:41 +0200

    Agent entity deleted

--- backend/travelAgent/src/main/java/com/crishof/travelagent/repository/AgentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crishof.travelagent.repository;
-
-import com.crishof.travelagent.model.Agent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AgentRepository extends JpaRepository<Agent, Long> {
-}
