7ee1cab054609dc521b22c11a78439cdb32ca32b
Cristian Hoffmann <cristianhof@gmail.com>
2025-07-02 23:01:05 +0200

    devtools to false on prod

--- backend/travelAgent/src/main/java/com/crishof/travelagent/TravelAgentApplication.java
+++ backend/travelAgent/src/main/java/com/crishof/travelagent/TravelAgentApplication.java
@@ -1,5 +1,6 @@
 package com.crishof.travelagent;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelAgentApplication {
 
     public static void main(String[] args) {
+        try {
         SpringApplication.run(TravelAgentApplication.class, args);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @PostConstruct
+    public void logStartup() {
+        System.out.println(">>> Aplicacion iniciada correctamente");
+        System.out.println("true = " + true);
+    }
 }
