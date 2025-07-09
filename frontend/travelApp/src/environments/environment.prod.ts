3ef3a6d72e634456376e6aa353ca4ad53f2a03ea
Cristian Hoffmann <cristianhof@gmail.com>
2025-07-09 10:36:22 +0200

    new backend url

--- frontend/travelApp/src/environments/environment.prod.ts
+++ frontend/travelApp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-production-715f.up.railway.app/api'
+  // apiUrl: 'https://backend-production-715f.up.railway.app/api'
+  apiUrl: 'https://travelagent.onrender.com'
 };
